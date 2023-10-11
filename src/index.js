c5b7194155abf2cc5882e312aed18b26f7183751
vinit jogi <vinit.jogi@somaiya.edu>
2023-10-11 18:29:17 +0530

    Basic setup done

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const { PORT, DB_SYNC } = require('./config/serverConfig');
+const apiRoutes = require('./routes/index');
+const db = require('./models/index');
+
+
+const setUpAndStartServer = () => {
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended : true}));
+
+    app.use('/api', apiRoutes);
+
+    app.listen(PORT, () => {
+        console.log(`server started on port ${PORT}`);
+    });
+
+    if(process.env.DB_SYNC){
+        db.sequelize.sync({alter : true});
+    }
+}
+
+setUpAndStartServer();
