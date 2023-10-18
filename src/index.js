ff13209a866c4d20c82f49c489401967bdb2800b
vinit jogi <vinit.jogi@somaiya.edu>
2023-10-18 15:40:10 +0530

    added api gateway functionality

--- src/index.js
+++ src/index.js
@@ -10,8 +10,12 @@ const setUpAndStartServer = () => {
 
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended : true}));
-
-    app.use('/api', apiRoutes);
+    // app.get('/bookingservice/api/v1/home', (req,res)=> {
+    //     return res.json({
+    //         message : 'hitting the booking service'
+    //     })
+    // })
+    app.use('/bookingservice/api', apiRoutes);
 
     app.listen(PORT, () => {
         console.log(`server started on port ${PORT}`);
